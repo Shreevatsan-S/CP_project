5e3b3b6233b7c2219c3fbd7ff0139041c9188750
Shreevatsan-S <133779156+Shreevatsan-S@users.noreply.github.com>
2023-07-01 21:05:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 
 app.post('/calculateSum', (req, res) => {
     try {
-        const { size, array } = req.body;
+        const { sum } = req.body;
         const childProcess = spawn('./a.out', []);
 
         let result = '';
@@ -27,17 +27,28 @@ app.post('/calculateSum', (req, res) => {
 
         childProcess.on('close', (code) => {
             if (code === 0) {
-                res.json({ result: parseInt(result) });
+                res.send(`
+                    <!DOCTYPE html>
+                    <html>
+                        <head>
+                            <title>Array Sum Calculator</title>
+                            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+                        </head>
+                        <body>
+                            <div class="container">
+                                <h1 class="text-center">Array Sum Calculator</h1>
+                                <div class="alert alert-success" role="alert">
+                                    The total amount of water trapped is ${parseInt(result)}
+                                </div>
+                            </div>
+                        </body>
+                    </html>
+                `);
             } else {
                 res.status(500).json({ error: 'An error occurred while calculating the sum.' });
             }
         });
-
-        // Pass the size and array as command line arguments to the C program
-        childProcess.stdin.write(`${size}\n`);
-        array.forEach((element) => {
-            childProcess.stdin.write(`${element}\n`);
-        });
+        childProcess.stdin.write(`${sum}\n`);
         childProcess.stdin.end();
     } catch (error) {
         console.error(`Error occurred: ${error}`);
